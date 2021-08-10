72963c1020b3cf5048cf1e8a919bda3fd5fdb1e6
lucas <lucasgominhaa448@gmail.com>
2021-08-10 12:57:08 -0300

    :fire: added new changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,17 +4,21 @@ const form = document.querySelector("form");
 btnSubmit.addEventListener("click", (event) => {
   event.preventDefault();
 
-  const fields = [...document.querySelectorAll(".input-block input")];   //pegando todos os input e colocando dentro de um array;
+  const fields = [...document.querySelectorAll(".input-block input")]; //pegando todos os input e colocando dentro de um array;
 
-  fields.forEach((field) => {   //varrendo o array e fazendo uma validacao se algum estiver vazio
-    if (field.value === "") {   //se estiver vazio vai add a classe na linha 13 no form;
+  fields.forEach((field) => {
+    //varrendo o array e fazendo uma validacao se algum estiver vazio
+    if (field.value === "") {
+      //se estiver vazio vai add a classe na linha 13 no form;
       form.classList.add("validate-error");
     }
   });
 
   const formError = document.querySelector(".validate-error");
-  if (formError) { //isso forma meio que um loop para quando a animacao acabar ela estar disponivel dnv
-    formError.addEventListener("animationend", (event) => { //quando chegar no final da animacao
+  if (formError) {
+    //isso forma meio que um loop para quando a animacao acabar ela estar disponivel dnv
+    formError.addEventListener("animationend", (event) => {
+      //quando chegar no final da animacao
       if (event.animationName === "nono") {
         formError.classList.remove("validate-error");
       }
