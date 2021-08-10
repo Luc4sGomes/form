3c9bfa57e8c7495db2f351e5970e78664ce7b65b
lucas <lucasgominhaa448@gmail.com>
2021-08-10 12:40:47 -0300

    :fire: added new events in js

---
+++ assets/scripts/script.js
@@ -0,0 +1,59 @@
+const btnSubmit = document.querySelector(".btn-login");
+const form = document.querySelector("form");
+
+btnSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const fields = [...document.querySelectorAll(".input-block input")];   //pegando todos os input e colocando dentro de um array;
+
+  fields.forEach((field) => {   //varrendo o array e fazendo uma validacao se algum estiver vazio
+    if (field.value === "") {   //se estiver vazio vai add a classe na linha 13 no form;
+      form.classList.add("validate-error");
+    }
+  });
+
+  const formError = document.querySelector(".validate-error");
+  if (formError) { //isso forma meio que um loop para quando a animacao acabar ela estar disponivel dnv
+    formError.addEventListener("animationend", (event) => { //quando chegar no final da animacao
+      if (event.animationName === "nono") {
+        formError.classList.remove("validate-error");
+      }
+    });
+  } else {
+    form.classList.add("form-hide");
+  }
+});
+
+form.addEventListener("animationend", (event) => {
+  if (event.animationName === "down") {
+    form.style.display = "none";
+    document.querySelector("body").style.overflow = "none";
+  }
+});
+
+form.addEventListener("animationstart", (event) => {
+  if (event.animationName === "down") {
+    document.querySelector("body").style.overflow = "hidden";
+  }
+});
+
+/*const btnSubmit = document.querySelector(".btn-login");
+const form = document.querySelector("form");
+
+btnSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+  form.classList.add("form-hide");
+});
+
+form.addEventListener("animationend", (event) => { //sera executado quando terminar a animacao
+  if (event.animationName === "down") { //se possuir a animacao com nome down
+    form.style.display = "none"; 
+    document.querySelector("body").style.overflow = "none";
+  }
+});
+
+form.addEventListener("animationstart", (event) =>{ //sera executado quando a animacao startar
+    if(event.animationName ==="down"){ //se possuir a animacao com nome down
+        document.querySelector("body").style.overflow = "hidden";
+    }
+});*/
